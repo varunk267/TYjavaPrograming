137e522a88e1c555afac940b4ad5ce6224f4aec9
Nithin1971 <nithingowda6677@gmail.com>
2021-03-22 22:00:03 +0530

    add

---
+++ TY_programing/src/Integer/PrintOddNumber17.java
@@ -0,0 +1,16 @@
+package Integer;
+
+public class PrintOddNumber17 {
+	public static void main(String[] args) {
+
+		for(int i=11; i<=20; i++){
+			if(i%2!=0){
+				if(i==17){
+					System.out.println("seventeen");
+				}else{
+					System.out.println(i);
+				}
+			}
+		}
+	}
+}
