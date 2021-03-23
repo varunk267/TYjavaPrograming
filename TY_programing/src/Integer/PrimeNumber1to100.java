255e70c88eea7d8aec53c1ef0f3f52a299b68825
Nithin1971 <nithingowda6677@gmail.com>
2021-03-23 09:25:05 +0530

    add

--- TY_programing/src/Integer/PrimeNumber1to100.java
+++ TY_programing/src/Integer/PrimeNumber1to100.java
@@ -4,7 +4,7 @@ public class PrimeNumber1to100 {
 
 	public static void main(String[] args) {
 		int num=100;
-		for(int i=0; i<=100; i++){
+		for(int i=0; i<=num; i++){
 			boolean flag=true;
 			for(int j=2; j<i; j++){
 				if(i%j==0){
