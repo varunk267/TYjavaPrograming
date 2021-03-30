5a29ac256f9561c7d06ed152f14d6dae44657e1b
varunk267 <varun.varu2671992@gmail.com>
2021-03-30 11:57:34 +0530

    conflict

--- TY_programing/src/Array_Prog/nitin.java
+++ TY_programing/src/Array_Prog/nitin.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package Array_Prog;
 
 public class nitin {
@@ -6,9 +6,4 @@ public static void main(String[] args) {
 	System.out.println("kjvbksjbvskjvb");
 }
 }
-=======
-class nitin
-{
-  
-}
->>>>>>> branch 'ahvvba' of https://github.com/varunk267/TYjavaPrograming.git
+
